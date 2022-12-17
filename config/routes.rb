94ec40330401c95c944077355243b3e8cfb4bbce
bruno chehboun <bruno.chehboun@gmail.com>
2022-12-17 17:14:10 +0100

    rails new - 0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   # Read all
+#   get 'tasks', to: 'tasks#index'
+#   # Create
+#   get    'tasks/new',      to: 'tasks#new',  as: :new_task
+#   post   'tasks',          to: 'tasks#create'
+#   # Read one
+#   get 'tasks/:id', to: 'tasks#show', as: :task
+#   # Update
+#   get    'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+#   patch  'tasks/:id',      to: 'tasks#update'
+#   #Delete
+#   delete 'tasks/:id', to: 'tasks#destroy'
+#   # Defines the root path route ('/')
+#   # root 'articles#index'
+# end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  resources :tasks
 end
